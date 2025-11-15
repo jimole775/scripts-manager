d3d706d31a657a618bbacd5b0d871a2f83eb9d4a
Rongxis <727346595@qq.com>
2025-11-16 01:00:52 +0800

    Create index.vue

---
+++ src/renderer/src/pages/api-scanner/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 显示列表数据 -->
+  <div>板块1</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      initLoading: false
+    };
+  },
+  mounted() {},
+  methods: {
+    switchChanged() {}
+  }
+};
+</script>
