4a76f18bfcc0aac0587d0249048c64799d5cb1b6
Rongxis <727346595@qq.com>
2025-11-16 00:37:04 +0800

    Create App.vue

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Home from '@ui/pages/home/index.vue'
+import Progress from '@ui/components/progress-bar.vue'
+</script>
+
+<template>
+<div>
+  <Progress />
+  <Home />
+</div>
+</template>
